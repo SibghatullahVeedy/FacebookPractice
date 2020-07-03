a3ae0053d1a3c4f1eb91a9bbc298e3712b5bbd09
Sibghatullah Veedy <sibghatullahveedy@gmail.com>
2020-07-03 12:35:38 -0700

    FaceBook Practice Repo

---
+++ src/main/java/FacebookPractice/FacebookPractice/App.java
@@ -0,0 +1,13 @@
+package FacebookPractice.FacebookPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
